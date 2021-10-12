0bf3caea35978761ea5bb6c9354736411331142d
Jlbejarano662 <jasmin.1207@hotmail.com>
2021-10-11 19:37:09 -0500

    tengo un error, no se que hacer

--- hola.py
+++ hola.py
@@ -1,1 +1,9 @@
-print ("hola);
+numero = input ("pasa el número: ")
+cont=0
+for i in range (numero):
+    if numero % 0==0:
+        cont = cont + 1
+if cont ==2:
+    print("el numero ", numero, " es  primo ")
+else:
+    print("el numero ", numero, " no es primo") 
