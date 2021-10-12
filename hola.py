94a3fc003e98b3c6dce09d30e4fecd279175248a
Jlbejarano662 <jasmin.1207@hotmail.com>
2021-10-11 20:17:41 -0500

    mejora realizada y finalizado

--- hola.py
+++ hola.py
@@ -1,9 +1,9 @@
 numero = int(input ("pasa el número: "))
 cont=0
-for i in range (1,numero +1):
+for i in range (2,numero +1):
     if numero % i==0:
         cont = cont + 1
-if cont ==2:
+if cont ==1:
     print("el numero ", numero, " es  primo ")
 else:
     print("el numero ", numero, " no es primo") 
